763ee8eb358b774032d9085516daebf16658c211
Nguyen Duc Hieu <ndhieu1994@gmail.com>
2018-02-25 13:17:11 +0100

    googleplot: added code option for axis limit (l:41-42)

--- scholar.google.com/googleplot.py
+++ scholar.google.com/googleplot.py
@@ -7,29 +7,40 @@ import traceback
 import matplotlib.pylab as plt
 
 print('Google scholar plotting')
+
+# directories which store json output data
 directiories = [
     'data-backups',
     'data-backups-1'
 ]
+
 try:
     years = list(range(1920, 2018))
     handles = []
-    # directory = 'data-backups'
     for directory in directiories:
         for fn in os.listdir(directory):
             json_data = json.load(open(directory + '/' + fn))
             result_data = []
             for year in years:
                 result_data.append(json_data['results'][str(year)])
+
+            # naming lines in legends
             label = directory + '/' + str(fn) + ' ' + json_data['datetime']
+
             line, = plt.plot(years, result_data, label=label)
             handles.append(line)
 
-    plt.legend(handles=handles)
+    plt.legend(handles=handles) # comment out if you don't want legends
 
     ticks = list(range(min(years), max(years)+1, 5))
     print('Ticks', ticks)
     plt.xticks(ticks)
+
+    # set plotting limit of x and y axis
+    # set one value in tuple for only limiting min-x or min-y value
+    # plt.xlim( (2010, 2016) )
+    # plt.ylim( (2000000) )
+
     # plt.savefig('googlescholar_plot.png')
     plt.show()
 
