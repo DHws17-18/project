29b5bc4470d9304a548933f6b1b1ef59244f0483
r2u9 <rene2u9@gmail.com>
2018-02-27 23:10:47 +0200

    github plot

--- github.com/analyze.py
+++ github.com/analyze.py
@@ -5,52 +5,33 @@ import json
 
 import matplotlib.pylab as plt
 
-year = 2009
-
-x = []
-y = []
+placeholder = [ 'github.ai', 'github.artificial', 'github.deep', 'github.machine' ] #, 'github.repos' ]
+handles = []
 
 if __name__ == '__main__':
-  #loop over each file in /github.repos
-  for root, dirs, files in os.walk('./github.repos/'):
-    for file in files:
-      #check if its json
-      if file.endswith('.json'):
-        #read lines
-        with open('./github.repos/' + file) as githubfile:
-          d = json.load(githubfile)
-          githubfile.close()
-          year += 1
-          print(d["total_count"])
-          x.append(year)
-          y.append(d["total_count"])
-#          for repo in d["items"]:
-#              #repoJson = json.loads(repo)
-#              print(repo["created_at"])
-  plt.plot(x, y)
-  plt.show()
-
-#TODO: 
-  year = 2009
-
-  x = []
-  y = []
   
-  #loop over each file in /githubdata
-  for root, dirs, files in os.walk('./github.ai/'):
-    for file in files:
-      #check if its json
-      if file.endswith('.json'):
-        #read lines
-        with open('./github.ai/' + file) as githubfile:
-          c = json.load(githubfile)
-          githubfile.close()
-          year += 1
-          print(c["total_count"])
-          x.append(year)
-          y.append(c["total_count"])
-#          for repo in d["items"]:
-#              #repoJson = json.loads(repo)
-#              print(repo["created_at"])
-  plt.plot(x, y)
+  for foldername in placeholder:
+    x = []
+    y = []
+    year = 2009
+    #loop over each file in /github.repos
+    for root, dirs, files in os.walk('./'+foldername):
+      for file in files:
+        #check if its json
+        if file.endswith('.json'):
+          #read lines
+          with open('./' + foldername + '/' + file, encoding='utf-8') as githubfile:
+            d = json.load(githubfile)
+            githubfile.close()
+            year += 1
+            print(d["total_count"])
+            x.append(year)
+            y.append(d["total_count"])
+#            for repo in d["items"]:
+#                #repoJson = json.loads(repo)
+#                print(repo["created_at"]))
+      line, = plt.plot(x, y, label=foldername.replace('github.', ''))
+      handles.append(line)
+
+  plt.legend(handles=handles)
   plt.show()
